6300e6c09a09b8f76d448fe43657ea37f0bbae2f
carmunoz <carmunoz@ff8bdbf7-185e-d3d2-476c-5af599394111>
2011-10-30 06:13:53 +0000

    Se comentó el sleep() que ya no hace falta

--- src/hospital/protected/pages/Home.php
+++ src/hospital/protected/pages/Home.php
@@ -4,7 +4,7 @@
 	class Home extends TPage {
 		public function clickCambiar($sender, $param) {
 			if( $this->Page->isValid ) {
-				sleep(5);
+				//sleep(5);
 				// $sender es el componente boton
 				$rate = floatval($this->cambioMoneda->Text);
 				$valorACambiar = floatval($this->cantidadACambiar->Text);
