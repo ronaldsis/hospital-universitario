5164d24e570cbcb04e10bf59edc9e590f1276c5a
aleshivan@gmail.com <aleshivan@gmail.com@ff8bdbf7-185e-d3d2-476c-5af599394111>
2011-11-12 18:11:37 +0000

    - se despliega mensaje de error en caso de fallas en la autenticacion - mensaje de error cuando la clave o el usuario estan equivocados

--- trunk/src/hospital/protected/pages/Login.php
+++ trunk/src/hospital/protected/pages/Login.php
@@ -3,16 +3,21 @@
 		public function entrar($sender, $param) {
 			if( $this->Page->IsValid ) {
 				$manager = $this->Application->Modules['users'];
+				try{
+					if( $manager->validateUser( $this->username->Text, $this->password->Text ) ) {
+						$user = $manager->getUser( $this->username->Text );
 
-				if( $manager->validateUser( $this->username->Text, $this->password->Text ) ) {
-					$user = $manager->getUser( $this->username->Text );
+						$auth = $this->Application->Modules['auth'];
+						$auth->updateSessionUser($user);
+						$this->Application->User = $user;
 
-					$auth = $this->Application->Modules['auth'];
-					$auth->updateSessionUser($user);
-					$this->Application->User = $user;
-
-					$url = $this->Service->constructUrl( $this->Service->DefaultPage );
-					$this->Response->redirect( $url );
+						$url = $this->Service->constructUrl( $this->Service->DefaultPage );
+						$this->Response->redirect( $url );
+					} else {
+						$this->mensaje->Text = "Nombre de usuario o clave incorrectas";
+					}					
+				} catch(TDbException $e) {
+					$this->mensaje->Text = "Hubo un error durante la autenticación, contacte al administrador";
 				}
 			}
 		}
